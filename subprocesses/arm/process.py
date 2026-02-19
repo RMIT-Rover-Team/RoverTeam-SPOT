dd933e4d52fa091215f6972fe109f509a952adf1
Aston <astonbolwell@gmail.com>
2026-02-19 20:21:45 +1100

    Update process.py

--- subprocesses/arm/process.py
+++ subprocesses/arm/process.py
@@ -103,12 +103,17 @@ async def telemetry_loop(interval: float):
         print(f"JSON {json.dumps({'type': 'arm', 'data': data})}")
         await asyncio.sleep(interval)
 
+async def heartbeat_loop(interval: float):
+    while True:
+        # Could be used for simple logging/debug
+        print("HEARTBEAT")
+        await asyncio.sleep(interval)
 
 # -------------------------
 # MAIN
 # -------------------------
 
-async def main(status_interval: float, ws_host: str, ws_port: int):
+async def main(heartbeat: float, sub_url: str, status_interval: float, ws_host: str, ws_port: int):
 
     receiver = Receiver(lambda msg: handle_gamepad_message(msg, receiver))
     gamepad_server = GamepadServer(ws_host, ws_port, receiver, sender_agents=actuators)
@@ -116,6 +121,7 @@ async def main(status_interval: float, ws_host: str, ws_port: int):
     await gamepad_server.start()
 
     tasks = [
+        asyncio.create_task(heartbeat_loop(heartbeat)),
         asyncio.create_task(telemetry_loop(status_interval))
     ]
 
@@ -136,9 +142,11 @@ async def main(status_interval: float, ws_host: str, ws_port: int):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--heartbeat", type=float, default=10)
     parser.add_argument("--status_interval", type=float, default=0.2)
     parser.add_argument("--ws_host", type=str, default="0.0.0.0")
+    parser.add_argument("--sub_url", type=str)
     parser.add_argument("--ws_port", type=int, default=8766)
     args = parser.parse_args()
 
-    asyncio.run(main(args.status_interval, args.ws_host, args.ws_port))
+    asyncio.run(main(args.heartbeat, args.sub_url, args.status_interval, args.ws_host, args.ws_port))
