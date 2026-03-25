a1205a38592f126654ed4cb7f0627797b26f1cec
Aston <astonbolwell@gmail.com>
2026-03-26 03:32:48 +1030

    Update stream_linux.py

--- subprocesses/cameras/stream_cameras/stream_linux.py
+++ subprocesses/cameras/stream_cameras/stream_linux.py
@@ -71,11 +71,12 @@ class CameraBroadcaster:
                 self.device,
                 format="v4l2",
                 options={
-                    "video_size": f"{self.width}x{self.height}",
+                    #"video_size": f"{self.width}x{self.height}",
                     "fflags": "nobuffer",  # Prevent ffmpeg from queuing old frames
                     "flags": "low_delay",  # Enforce low latency mode
                 },
             )
+            
             stream = container.streams.video[0]
 
             for frame in container.decode(stream):
